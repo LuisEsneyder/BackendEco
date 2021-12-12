3d6c5a8e0dad291b63bd6314069eefb568d48cd1
Luis Fernández <luisexneider1999@gmail.com>
2021-12-12 14:22:48 -0500

    despliegue pueba #50000

--- src/strategies/client.strategies.ts
+++ src/strategies/client.strategies.ts
@@ -1,19 +1,16 @@
 import {AuthenticationStrategy} from "@loopback/authentication";
 import {service} from "@loopback/core";
 import {HttpErrors, Request} from "@loopback/rest";
-import {UserProfile} from '@loopback/Security';
+import {UserProfile} from "@loopback/security";
 import parseBearerToken from "parse-bearer-token";
 import {AutenticacionService} from "../services";
-
-export class ClientStrategy implements AuthenticationStrategy {
+export class AdminStrategy implements AuthenticationStrategy {
     name: string = 'cliente';
 
     constructor(
         @service(AutenticacionService)
-        public servicioAutenticacion: AutenticacionService
-    ) {
-
-    }
+        public servicioAutenticacion: AutenticacionService,
+    ) { }
 
     async authenticate(request: Request): Promise<UserProfile | undefined> {
         let token = parseBearerToken(request);
@@ -28,10 +25,10 @@ export class ClientStrategy implements AuthenticationStrategy {
                 return perfil;
 
             } else {
-                throw new HttpErrors[401]("El token incluido no es valido.")
+                throw new HttpErrors[401]('El token incluido no es valido.');
             }
         } else {
-            throw new HttpErrors[401]("No se ha incluido un token en la solicitud.")
+            throw new HttpErrors[401]('No se ha incluido un token en la solicitud.');
         }
     }
 }
--- src/strategies/employ.strategies.ts
+++ src/strategies/employ.strategies.ts
@@ -1,19 +1,16 @@
 import {AuthenticationStrategy} from "@loopback/authentication";
 import {service} from "@loopback/core";
 import {HttpErrors, Request} from "@loopback/rest";
-import {UserProfile} from '@loopback/Security';
+import {UserProfile} from "@loopback/security";
 import parseBearerToken from "parse-bearer-token";
 import {AutenticacionService} from "../services";
-
-export class EmployeeStrategy implements AuthenticationStrategy {
+export class AdminStrategy implements AuthenticationStrategy {
     name: string = 'employee';
 
     constructor(
         @service(AutenticacionService)
-        public servicioAutenticacion: AutenticacionService
-    ) {
-
-    }
+        public servicioAutenticacion: AutenticacionService,
+    ) { }
 
     async authenticate(request: Request): Promise<UserProfile | undefined> {
         let token = parseBearerToken(request);
@@ -26,11 +23,12 @@ export class EmployeeStrategy implements AuthenticationStrategy {
                     id: datos.data.id
                 });
                 return perfil;
+
             } else {
-                throw new HttpErrors[401]("El token incluido no es valido.")
+                throw new HttpErrors[401]('El token incluido no es valido.');
             }
         } else {
-            throw new HttpErrors[401]("No se ha incluido un token en la solicitud.")
+            throw new HttpErrors[401]('No se ha incluido un token en la solicitud.');
         }
     }
 }
